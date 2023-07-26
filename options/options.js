5e85b47c7fc67d32b035785521e38a123941d61f
Andy Hughes <andyforhogs@gmail.com>
2023-07-25 20:45:14 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "ct-ar-42429",
-  url: "https://ct-ar-42429.botics.co",
-  api: "https://ct-ar-42429.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "ct-ar-42429",
+  "url": "https://ct-ar-42429.botics.co",
+  "api": "https://ct-ar-42429.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "EventsSimpleScreen3Copy"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
